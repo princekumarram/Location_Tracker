8ff48883323893e7f756be61476a41d514946280
Prince Kumar Ram <132006575+princekumarram@users.noreply.github.com>
2024-11-03 20:06:08 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+ 
+const socket = io();
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position) => {
+        const {latitude,longitude}=position.coords;
+        socket.emit("send-location",{latitude,longitude});
+        },
+        (error)=>{
+            console.error(error);
+        },{
+            enableHighAccuracy:true,
+            timeout:5000,
+            maximumAge:0
+        }
+    );
+}
+
+const map = L.map("map").setView([0,0],16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"Ludhiana"
+}).addTo(map);
+
+const markers={};
+
+socket.on("receive-location",(data)=>{
+    console.log("Received location data:", data);
+    const {id, latitude,longitude}=data;
+    map.setView([latitude,longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude,longitude]);
+    }else{
+        markers[id]=L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
